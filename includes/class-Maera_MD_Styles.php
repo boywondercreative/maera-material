ee3e0b43fb63557ebf018bd50485eed380ec628b
Aristeides Stathopoulos <aristath@gmail.com>
2014-12-07 04:38:16 +0200

    Add an 1-hour transient for color calculations

--- includes/class-Maera_MD_Styles.php
+++ includes/class-Maera_MD_Styles.php
@@ -7,26 +7,31 @@ class Maera_MD_Styles {
 		add_filter( 'maera/styles', array( $this, 'color_mods' ) );
 	}
 
-	function color_mods( $styles ) {
+	function color_mods( $css ) {
 
 		$colors = maera_md_colors();
+		$styles = get_transient( 'maera_md_colors' );
 
-		foreach ( $colors as $color => $classes ) {
+		if ( false === ( $styles ) ) {
 
-			$bg_obj  = new Jetpack_Color( '#' . $color );
-			$classes = '.' . str_replace( ' ', '.', $classes );
+			$styles = '';
 
-			$bg    = '#' . str_replace( '#', '', $bg_obj->toHex() );
-			$color = $bg_obj->getReadableContrastingColor( $bg_obj, 2 )->toHex();
+			foreach ( $colors as $color => $classes ) {
+				$bg_obj  = new Jetpack_Color( '#' . $color );
+				$classes = '.' . str_replace( ' ', '.', $classes );
 
-			$link_color = ( '000000' == $color ) ? 'rgba(0,0,0,.75)' : 'rgba(255,255,255,.75)';
+				$bg         = '#' . str_replace( '#', '', $bg_obj->toHex() );
+				$color      = '#' . $bg_obj->getReadableContrastingColor( $bg_obj, 2 )->toHex();
+				$link_color = ( '000000' == $color ) ? 'rgba(0,0,0,.75)' : 'rgba(255,255,255,.75)';
 
-			$styles .= $classes . '{background:#' . $bg . ';color:#' . $color . ';}';
-			$styles .= $classes . ' a{color:' . $link_color . ';}';
+				$styles .= $classes . '{background:' . $bg . ';color:' . $color . ';}' . $classes . ' a{color:' . $link_color . ';}';
+			}
+
+			set_transient( 'maera_md_colors', $styles, 60 * 60 );
 
 		}
 
-		return $styles;
+		return $css . $styles;
 
 	}
 
